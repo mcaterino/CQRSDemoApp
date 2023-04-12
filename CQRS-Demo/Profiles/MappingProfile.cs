2b93bb54b378b6c57aa0b75050fbfa90627e8176
Mauricio Caterino <mauricio_caterino@outlook.com>
2023-04-12 11:02:57 -0400

    added mapping profile for auto mapping

---
+++ CQRS-Demo/Profiles/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CQRS_Demo.DTOs.Product;
+using CQRS_Demo.Models;
+
+namespace CQRS_Demo.Profiles {
+    public class MappingProfile : Profile {
+
+        public MappingProfile()
+        {
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+
+
+
+    }
+}
