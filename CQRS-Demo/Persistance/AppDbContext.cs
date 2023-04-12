1d6e8d9e82641189fb6361fe8f1f3c7c47022712
Mauricio Caterino <mauricio_caterino@outlook.com>
2023-04-12 10:58:40 -0400

    added dbContext and initial migration

---
+++ CQRS-Demo/Persistance/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CQRS_Demo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRS_Demo.Persistance {
+    public class AppDbContext : DbContext, IAppDbContext {
+
+        public DbSet<Product> Products { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
+        }
+
+        public async Task<int> SaveChanges() {
+
+            return await base.SaveChangesAsync();
+        }
+    }
+}
---
+++ CQRS-Demo/Persistance/IAppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using CQRS_Demo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRS_Demo.Persistance {
+    public interface IAppDbContext {
+        DbSet<Product> Products { get; set; }
+
+        Task<int> SaveChanges();
+    }
+}
