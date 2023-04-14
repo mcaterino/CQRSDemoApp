b34ac370a88de66ab4c971a5d0ff9bc64e2f9ff2
Mauricio Caterino <mauricio_caterino@outlook.com>
2023-04-13 21:38:10 -0400

    registered service for IProductRepository

--- CQRS-Demo/Program.cs
+++ CQRS-Demo/Program.cs
@@ -1,7 +1,9 @@
+using CQRS_Demo.Contracts;
 using CQRS_Demo.Persistance;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,15 +12,33 @@ var cnnString = builder.Configuration.GetConnectionString("sqlve");
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(cnnString));
 
+
 builder.Services.AddControllers();
 
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+#region Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.IncludeXmlComments(string.Format(@"{0}\CQRS.WebApi.xml", System.AppDomain.CurrentDomain.BaseDirectory));
+    c.SwaggerDoc("v1", new OpenApiInfo {
+        Version = "v1",
+        Title = "CQRS.WebApi",
+    });
 
+});
+#endregion
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+#region Swagger
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "CQRS.WebApi");
+});
+#endregion
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
